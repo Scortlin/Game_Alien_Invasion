4025666588bb943c7194cf95135ba19a6aa058ee
Scortlin <vvvbykov@gmail.com>
2025-11-12 16:37:53 +0300

    game commit v2

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,12 +1,14 @@
 import sys
 
 import pygame
-
+from settings import Settings
 class AlienInvasion:
 
     def __init__(self):
         pygame.init()
-        self.screen = pygame.display.set_mode((1200, 800))
+        self.clock = pygame.time.Clock()
+        self.settings = Settings()
+        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien invasion")
 
     def run_game(self):
@@ -14,7 +16,9 @@ class AlienInvasion:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
+            self.screen.fill(self.settings.bg_color)
             pygame.display.flip()
+            self.clock.tick(60)
 
 if __name__ == '__main__':
     ai = AlienInvasion()
